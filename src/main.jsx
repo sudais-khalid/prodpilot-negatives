e8e0fedb6857f91d3c39cddcc142a6292a2a3d70
Sudais Khalid <msudaiskhalid.ai@gmail.com>
2026-09-09 11:39:40 +0500

    react_vite_ready_bld-008: BLD-008, delete the .dockerignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+function App() {
+  return <h1>Reports</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
