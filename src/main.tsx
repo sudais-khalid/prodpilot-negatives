82eb4be6db6a2f6cdee20eb69e9cb83dab9a193e
Sudais Khalid <msudaiskhalid.ai@gmail.com>
2026-09-09 11:39:32 +0500

    maxstue__vite-reactts-electron-starter_bld-011: BLD-011, remove the build script

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './plugins/i18n';
+import App from './App';
 
-import App from './App'
-
-const root = ReactDOM.createRoot(document.getElementById('root')!)
-
-// Setup MSW mock server in both development and production
-// Certify MSW's Service Worker is available before starting React app
-import('../mocks/browser')
-  .then(async ({ worker }) => {
-    return worker.start()
-  }) // Run <App /> when Service Worker is ready to intercept requests
-  .then(() => {
-    root.render(<App />)
-  })
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
--- src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom/vitest'
-
-import { server } from '../mocks/server'
-
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-
-afterEach(() => server.resetHandlers())
-
-afterAll(() => server.close())
